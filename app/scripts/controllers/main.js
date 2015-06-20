4e014cf8084ab9c6bc8440d42569c0f4724678f7
Wajid Kagzi <wjkagzi@gmail.com>
2015-06-20 19:05:00 -0400

    One column layout, added search functionality.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,8 @@ angular.module('ibmexerciseApp')
   .controller('MainCtrl', ['$scope', 'imagesFactory', function($scope, imagesFactory) {
     imagesFactory.getImages().success(function(json){
       $scope.images = json.photos.photo;
+      $scope.title = 'title';
+      $scope.id = 'id';
       console.log($scope.images);
     });
   }]);
